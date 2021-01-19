728b9efabac4204f0dd13d5844a9410d161ae948
Emily Zhai <emily.zhai@convoy.com>
2021-01-19 01:01:26 -0800

    starter commit

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import styled from '@emotion/styled';
+
+const Body = styled.body({
+  margin: 0,
+  fontFamily: 'Roboto, sans-serif',
+});
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fraunces&family=Oswald:wght@300&family=Roboto:wght@300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Body>
+          <Main />
+          <NextScript />
+        </Body>
+      </Html>
+    )
+  }
+}
+
+export default Document;
