cb53c931470bbdcd99c5a47f526eeae602967bd5
Emily Zhai <emily.zhai@convoy.com>
2021-01-25 23:59:26 -0800

    color palette

--- shared/styles.ts
+++ shared/styles.ts
@@ -2,3 +2,15 @@ const breakpoints = [767];
 export const mq = breakpoints.map(
   bp => `@media (max-width: ${bp}px)`,
 );
+
+export const Colors = {
+  BRAND: {
+    DARK: '#F53280',
+    LIGHT: '#FEADDC',
+  },
+  NEUTRALS: {
+    GRAY: '#E4E4E4',
+    BLACK: '#191919',
+    WHITE: '#FFFFFF',
+  },
+};
