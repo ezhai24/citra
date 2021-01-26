1a3af29bcd9d29d5b457d53bcc8dfaa5d7576c43
Emily Zhai <emily.zhai@convoy.com>
2021-01-25 23:59:42 -0800

    resuable button component

---
+++ components/Button.tsx
@@ -0,0 +1,40 @@
+import { ClassNames } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ReactChild } from 'react';
+
+import { Colors } from '~/shared/styles';
+
+const Root = styled.button(({ filled, primary }: any) => ({
+  borderStyle: 'solid',
+  borderColor: primary ? Colors.BRAND.DARK : Colors.NEUTRALS.BLACK,
+  padding: 10,
+  backgroundColor: filled
+    ? (primary ? Colors.BRAND.DARK : Colors.NEUTRALS.BLACK)
+    : Colors.NEUTRALS.WHITE,
+  color: filled
+    ? Colors.NEUTRALS.WHITE
+    : (primary ? Colors.BRAND.DARK : Colors.NEUTRALS.BLACK),
+}));
+
+interface Props {
+  primary?: boolean;
+  filled?: boolean;
+  onClick?: () => void;
+  children: ReactChild | ReactChild[];
+  className?: string;
+}
+
+const Button = ({ primary, filled, onClick, className, children }: Props) => {
+  return (
+    <Root
+      primary={primary}
+      filled={filled}
+      onClick={onClick}
+      className={className}
+    >
+      {children}
+    </Root>
+  );
+};
+
+export default Button;
